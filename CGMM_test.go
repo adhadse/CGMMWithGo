1d67bdd88cf3ed86c6a09859e6348caac45d0a22
Anurag Dhadse <hello@adhadse.com>
2021-10-28 16:02:33 +0530

    added animation function

--- CGMM_test.go
+++ CGMM_test.go
@@ -162,7 +162,7 @@ func TestCircleMidPoint(t *testing.T) {
 func TestDrawShape(t *testing.T) {
 	screenWidth := int32(xMax)
 	screenHeight := int32(yMax)
-	rl.InitWindow(screenWidth, screenHeight, "Bresenham algorithm | Computer Graphics Demo algorithms")
+	rl.InitWindow(screenWidth, screenHeight, "Shape drawing program | Computer Graphics Demo algorithms")
 	defer rl.CloseWindow()
 	winImage := rl.LoadImage("res/golang-48.png")
 	rl.SetWindowIcon(winImage)
@@ -170,14 +170,122 @@ func TestDrawShape(t *testing.T) {
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawCircle(40, 40, 30, rl.Black)
-		rl.DrawRectangle(90, 90, 40, 30, rl.Black)
-		rl.DrawEllipse(140, 140, 50, 20, rl.Black)
+		rl.DrawCircleLines(40, 40, 30, rl.Black)
+		rl.DrawRectangleLines(90, 90, 40, 30, rl.Black)
+		rl.DrawEllipseLines(140, 140, 50, 20, rl.Black)
+		rl.DrawLine(200, 200, 334, 442, rl.Black)
 		rl.EndDrawing()
 	}
 	rl.CloseWindow()
 }
 
+func TestAnimation(t *testing.T) {
+	screenWidth := int32(xMax)
+	screenHeight := int32(yMax)
+
+	rl.InitWindow(screenWidth, screenHeight, "Collision detection")
+	defer rl.CloseWindow()
+
+	// Box A: Moving box
+	boxA := rl.Rectangle{X: 10, Y: float32(rl.GetScreenHeight() / 2.0) - 50, Width:200, Height:100 }
+	var boxASpeedX float32 = 4
+
+	// Box B: Mouse moved box
+	boxB := rl.Rectangle{X:float32(rl.GetScreenWidth() / 2.0) - 30, Y:float32(rl.GetScreenHeight()/2.0) - 30,
+						 Width:60, Height: 60 }
+
+	boxCollision := rl.Rectangle{} // Collision rectangle
+
+	var screenUpperLimit float32 = 40      // Top menu limits
+
+	pause := false             // Movement pause
+	collision := false         // Collision detection
+
+	rl.SetTargetFPS(60)               // Set our game to run at 60 frames-per-second
+
+	for !rl.WindowShouldClose() { // Detect window close button or ESC key
+		// Move box if not paused
+		if !pause {
+			boxA.X += boxASpeedX
+		}
+
+		// Bounce box on x screen limits
+		if ((boxA.X + boxA.Width) >= float32(rl.GetScreenWidth())) || (boxA.X <= 0) {
+			boxASpeedX *= -1
+		}
+
+		// Update player-controlled-box (box02)
+		boxB.X = float32(rl.GetMouseX()) - boxB.Width/2
+		boxB.Y = float32(rl.GetMouseY()) - boxB.Height/2
+
+		// Make sure Box B does not go out of move area limits
+		if (boxB.X + boxB.Width) >= float32(rl.GetScreenWidth()) {
+			boxB.X = float32(rl.GetScreenWidth()) - boxB.Width
+		} else if boxB.X <= 0 {
+			boxB.X = 0
+		}
+
+		if (boxB.Y + boxB.Height) >= float32(rl.GetScreenHeight()) {
+			boxB.Y = float32(rl.GetScreenHeight()) - boxB.Height
+		} else if boxB.Y <= screenUpperLimit {
+			boxB.Y = screenUpperLimit
+		}
+
+		// Check boxes collision
+		collision = rl.CheckCollisionRecs(boxA, boxB)
+
+		// Get collision rectangle (only on collision)
+		if collision {
+			boxCollision = rl.GetCollisionRec(boxA, boxB)
+		}
+
+		// Pause Box A movement
+		if rl.IsKeyPressed(' ') {
+			pause = !pause
+		}
+
+		// Draw
+		//-----------------------------------------------------
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+
+		rec_color := rl.Red
+		if collision { rec_color = rl.Red } else { rec_color = rl.Black}
+
+		rl.DrawRectangle(0, 0, screenWidth, int32(screenUpperLimit), rec_color)
+
+		rl.DrawRectangleRec(boxA, rl.Gold)
+		rl.DrawRectangleRec(boxB, rl.Blue)
+		if collision {
+			// Draw collision area
+			rl.DrawRectangleRec(boxCollision, rl.Lime)
+
+			// Draw collision message
+			rl.DrawText("COLLISION!", rl.GetScreenWidth()/2 - rl.MeasureText("COLLISION!", 20)/2, int32(screenUpperLimit/2-10), 20, rl.Black)
+		}
+		rl.DrawFPS(10, 10)
+		rl.EndDrawing()
+	}
+}
+
+func TestFloodFill(t *testing.T) {
+	screenWidth := int32(xMax)
+	screenHeight := int32(yMax)
+	rl.InitWindow(screenWidth, screenHeight, "Flood fill algorithm | Computer Graphics Demo algorithms")
+	defer rl.CloseWindow()
+	winImage := rl.LoadImage("res/golang-48.png")
+	rl.SetWindowIcon(winImage)
+
+	box := rl.Rectangle{X: 10, Y: float32(rl.GetScreenHeight() / 2.0) - 50, Width:200, Height:100 }
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawRectangleLines(int32(box.X), int32(box.Y), int32(box.Height), int32(box.Width), rl.Black)
+		rl.EndDrawing()
+	}
+	rl.EndDrawing()
+}
 
 func TestCohenSutherlandClip(t *testing.T) {
 	l := LineInt{-3, -3, 5000, 4440}
